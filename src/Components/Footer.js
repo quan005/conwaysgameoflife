6382e8093a000ad7465437d710c7294a34d7c4f1
quan005 <ralphealfreeman@gmail.com>
2020-08-31 19:47:11 -0700

    Added additional styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,8 +8,15 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <div>
-                Footer
+            <div className='footer-container'>
+                <div className='title'>
+                    <h3>About This Algorithm:</h3>
+                </div>
+                <div className='about-container'>
+                    <p>
+                    Conway's Game of Life was created in 1970 by the British mathmetician, John Horton Conway. The player interacts with the game by creating an initial layout, and when executed, the game is capable of infinitely evolving. It is a turing complete algorithm that simulates cellular automata.
+                    </p>
+                </div>
             </div>
         )
     }
