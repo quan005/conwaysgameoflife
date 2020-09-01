d9c82f29a689564384e92d2f07b86a93e95ac2d4
quan005 <ralphealfreeman@gmail.com>
2020-08-31 18:17:07 -0700

    Added rules and presets

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import Grid from './Grid.js'
+import glider from './glider.svg'
+import lwss from './lwss.svg'
+import pulsar from './pulsar.svg'
+import pentadecathlon from './pentadecathlon.svg'
+import random from './random.svg'
 
 
 function arrClone(arr) {
@@ -14,7 +19,8 @@ class Body extends React.Component {
         this.rows = 25
         this.columns = 25
         this.state = {
-            speed: 100,
+            speed: 500,
+            value: '3',
             generation: 0,
             gridSetup: Array(this.rows).fill().map(() => Array(this.columns).fill(false)),
             presets: {
@@ -48,7 +54,7 @@ class Body extends React.Component {
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
                     ],
-                    image: ''
+                    image: glider
                 },
                 LWSS: {
                     name: 'Lightweight Spaceship',
@@ -80,7 +86,7 @@ class Body extends React.Component {
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
                     ],
-                    image: ''
+                    image: lwss
                 },
                 Pulsar: {
                     name: 'Pulsar',
@@ -112,7 +118,7 @@ class Body extends React.Component {
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
                     ],
-                    image: ''
+                    image: pulsar
                 },
                 Pentadecathlon: {
                     name: 'Pentadecathlon',
@@ -144,7 +150,7 @@ class Body extends React.Component {
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
                         [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
                     ],
-                    image: ''
+                    image: pentadecathlon
                 },
             }
         }
@@ -171,6 +177,7 @@ class Body extends React.Component {
         }
 
         this.setState({
+            generation: 0,
             gridSetup: newGrid
         })
     }
@@ -255,17 +262,62 @@ class Body extends React.Component {
         })
     }
 
-    presetHandler = () => {
-
+    presetHandler = (presetData) => {
+        // console.log(presetData)
+        this.setState({
+            generation: 0,
+            gridSetup: presetData
+        })
     }
 
-    slideHandler = (presetTitle) => {
-        
+    slideHandler = (event) => {
+        this.setState({
+            value: event.target.value
+        })
+
+        if(event.target.value === '5') {
+            this.setState({
+                speed: 50
+            }, () => {
+                console.log('speed 50', this.state.speed)
+            })
+        } else if(event.target.value === '4') {
+            this.setState({
+                speed: 250
+            }, () => {
+                console.log('speed 250', this.state.speed)
+            })
+        } else if (event.target.value === '3') {
+            this.setState({
+                speed: 500
+            }, () => {
+                console.log('speed 500', this.state.speed)
+            })
+        } else if (event.target.value === '2') {
+            this.setState({
+                speed: 750
+            }, () => {
+                console.log('speed 750', this.state.speed)
+            })
+        } else {
+            this.setState({
+                speed: 1000
+            }, () => {
+                console.log('speed 1000', this.state.speed)
+            })
+        }
     }
 
 
     componentDidMount() {
         this.randomizer();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.speed !== this.state.speed) {
+            clearInterval(this.intervalId)
+            this.intervalId = setInterval(this.start, this.state.speed)
+        }
     }
 
     render() {
@@ -286,40 +338,55 @@ class Body extends React.Component {
                             />
 
                             <div className='control-container'>
-                                <div>
-                                    <button onClick={this.startHandler}>Start</button>
-                                    <button onClick={this.pauseHandler}>Pause</button>
-                                    <button onClick={this.clearHandler}>Clear</button>
-                                </div>
-                                <div>
+                                <div className='slider-container'>
                                     <input
                                         type='range'
                                         min='1'
-                                        max='100'
-                                        value='50'
+                                        max='5'
                                         className='slider-input'
+                                        value={this.state.value}
                                         onChange={this.slideHandler}
                                     />
+                                </div>
+
+                                <div className='button-container'>
+                                    <button className='button' onClick={this.startHandler}>Start</button>
+                                    <button className='button' onClick={this.pauseHandler}>Pause</button>
+                                    <button className='button' onClick={this.clearHandler}>Clear</button>
                                 </div>
                             </div>
                         </div>
                         
                         <div className='preset-container'>
-                            {this.state.presets.map((preset, i) => (
-                                <div className='img-container'>
-                                    <img src={preset.image} alt={preset.name} key={i} />
+                            {Object.entries(this.state.presets).map(([key, value], i) => (
+                                <div className='img-container' onClick={e => this.presetHandler(value.data)} key={i}>
+                                    <img src={value.image} alt={value.name} width='65px' />
                                     <div className='info-container'>
-                                        <h2>{preset.name}</h2>
-                                        <p>{preset.type}</p>
+                                        <h2 className='info-name'>{value.name}</h2>
                                     </div>
                                 </div>
                             ))}
+                            <div className='img-container' onClick={e => this.randomizer()}>
+                                    <img src={random} alt='Random' width='65px' />
+                                    <div className='info-container'>
+                                        <h2 className='info-name'>Random</h2>
+                                    </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className='right-container'>
                     <div className='title'>
                         <h3>Rules:</h3>
+                    </div>
+
+                    <div className='rules-container'>
+                        <ul>
+                            <li>Any live cell with fewer than two live neighbours dies</li>
+                            <li>Any live cell with two or three live neighbours lives on to the next generation</li>
+                            <li>Any live cell with more than three live neighbours dies</li>
+                            <li>Any dead cell with exactly three live neighbours becomes a live cell</li>
+                        </ul>
                     </div>
                 </div>
             </div>
